e96a059382ce97c7be6b63dcc10168fcc40fae77
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2022-04-27 18:33:35 +0100

    commits made to labtest branch

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -6,36 +6,54 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
-public class NematodeVisualiser extends PApplet
-{
+public class NematodeVisualiser extends PApplet {
 
-	public void keyPressed()
-	{		
-		if (keyCode == LEFT)
-		{
+	// Create array list composed of only nematode-classed objects
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
+	public void keyPressed() {	
+		int count = 0;	
+		if (keyCode == LEFT) {
+			count--;
+			if (count < 0) {
+                count = count % nematodes.size();
+            }
+			draw(count);
 		}		
+		if (keyCode == RIGHT) {
+			count++;
+			if (count > nematodes.size()) {
+            	count = count % nematodes.size();
+            }
+			draw(count);
+		}
 	}
 
 
-	public void settings()
-	{
+	public void settings() {
 		size(800, 800);
 	}
 
-	public void setup() 
-	{
+	public void setup() {
 		colorMode(HSB);
 		background(0);
 		smooth();				
 	}
 	
 
-	public void loadNematodes()
-	{
+	public void loadNematodes() {
+		Table file = loadTable("nematodes.csv", "header");
+		for (TableRow r:file.rows()) {
+			Nematode n = new Nematode(r);
+			nematodes.add(n);
+		}
 	}
 
+	public void draw(int count) {
+		drawNematode(count);
+	}
 
-	public void draw()
-	{	
+	public void drawNematode(int count) {
+		nematodes.get(count).render(this);
 	}
 }
